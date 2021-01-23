bc99d4359b328e0f82c732888333c5f8a3405076
TunYi Chang <tunyic@uci.edu>
2021-01-23 10:21:23 +0800

    doing loop?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ class Main{
 	
 	public static void main(String[] args) {
 		
-		String pointer = "/note/to/random";
+		String pointer = "/note/to";
 		File xml = new File("simpleXML.xml");
 		FileReader reader;
 		try {
@@ -34,26 +34,36 @@ class Main{
 
 			System.out.println(Arrays.toString(pathArr));
 	        boolean found = false;
+	        int i = 0; // record the position in pathArr
+	        String tag = pathArr[0];
 
 	        while(x.more()) {
 				while (!found) {
 					x.skipPast("<");
 					if (x.more()) {
 						//System.out.println(x.nextToken());
-						if (x.nextToken().equals("note")) {
-							found = true;
+						if (x.nextToken().equals(tag)) {
+							i++;
+							tag = pathArr[i];
+							if(i == pathArr.length-1) {
+								System.out.println(tag);
+								found = true;
+							}
+								
 						}
 					}
 				}
 				if (found) {
 					x.skipPast("<");
 					if (x.more()) {
-						if(XML.parse(x, jo, "note", XMLParserConfiguration.ORIGINAL))
+						System.out.println(tag);
+						if(XML.parse(x, jo, tag, XMLParserConfiguration.ORIGINAL))
 							break;
 							
 					}
 				}
 			}
+	        
 	        System.out.println(jo.toString(4));
 	        
 			
