b86165531e9dd097bf599bfcd519b551e3c36dc9
TunYi Chang <tunyic@uci.edu>
2021-01-23 10:02:53 +0800

    modified by Clare

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,13 @@ class Main{
 	public static void main(String[] args) {
 		
 //		String pointer = "/note/to/random";
-		File xml = new File("simpleXML.xml");
+		File xml = new File("a.xml");
 		FileReader reader;
 		try {
 			reader = new FileReader(xml);
 			JSONObject jo = new JSONObject();
 	        XMLTokener x = new XMLTokener(reader);
-	        System.out.println(x);
+	        //System.out.println(x);
 	        boolean found = false;
 
 	        while(x.more()) {
@@ -21,19 +21,26 @@ class Main{
 					x.skipPast("<");
 					if (x.more()) {
 						//System.out.println(x.nextToken());
-						if (x.nextToken().equals("note")) {
+						if (x.nextToken().equals("Name")) {
 							found = true;
 						}
 					}
 				}
 				if (found) {
 					x.skipPast("<");
-					if (x.more())
-						XML.parse(x, jo, "note", XMLParserConfiguration.ORIGINAL);
+					if (x.more()) {
+						if(XML.parse(x, jo, "Name", XMLParserConfiguration.ORIGINAL))
+							break;
+							
+					}
+						
+					
 
 				}
 			}
-			System.out.println(jo.toString(4));
+	        System.out.println(jo.toString(4));
+	        
+			
 
 
 
